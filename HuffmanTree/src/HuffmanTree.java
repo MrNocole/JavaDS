f3fba64e896eab10662b37ed027643b14dcafd8e
MrNocole <1926276913@qq.com>
2021-03-31 09:32:05 +0800

    update

---
+++ HuffmanTree/src/HuffmanTree.java
@@ -0,0 +1,51 @@
+package src;
+
+import java.util.*;
+
+public class HuffmanTree {
+    List<Map.Entry<Character,Integer>> info ;
+    TreeNode head;
+    PriorityQueue<TreeNode> priorityQueue;
+    HuffmanTree(Map<Character,Integer> map){
+        head = new TreeNode();
+        info = new ArrayList<>(map.entrySet());
+        Collections.sort(info, new Comparator<>() {
+            @Override
+            public int compare(Map.Entry<Character, Integer> o1, Map.Entry<Character, Integer> o2) {
+                return o1.getValue() - o2.getValue();
+            }
+        });
+        priorityQueue = new PriorityQueue<>();
+        for (Map.Entry<Character,Integer> entry : info){
+            TreeNode treeNode = new TreeNode.builder().setVal(entry.getValue()).setChar(entry.getKey()).build();
+            priorityQueue.offer(treeNode);
+        }
+    }
+
+    public void DoHuffman(){
+        while (priorityQueue.size()>1){
+            TreeNode l = priorityQueue.poll();
+            TreeNode r = priorityQueue.poll();
+            TreeNode tmp =  new TreeNode.builder().setVal(l.val+r.val).setLeft(l).setRight(r).build();
+            priorityQueue.offer(tmp);
+        }
+        head = priorityQueue.poll();
+    }
+
+    public void dfs(TreeNode node, StringBuffer crt){
+        if (node.left==null && node.right==null) {
+            System.out.println(node.c + " " + crt.toString());
+            return;
+        }
+        dfs(node.left,new StringBuffer(crt+"0"));
+        dfs(node.right,new StringBuffer(crt+"1"));
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.next();
+        StringDealer stringDealer = new StringDealer(s);
+        HuffmanTree huffmanTree = new HuffmanTree(stringDealer.Deal());
+        huffmanTree.DoHuffman();
+        huffmanTree.dfs(huffmanTree.head,new StringBuffer());
+    }
+}
---
+++ HuffmanTree/src/TreeNode.java
@@ -0,0 +1,45 @@
+package src;
+
+public class TreeNode implements Comparable<TreeNode>{
+    int val;
+    char c;
+    TreeNode left ,  right;
+    TreeNode (){
+
+    }
+    TreeNode (builder builder){
+        this.val = builder.val;
+        this.left = builder.left;
+        this.right = builder.right;
+        this.c = builder.c;
+    }
+    public static class builder{
+        int val;
+        TreeNode left , right;
+        char c;
+        public builder(){}
+        public builder setChar(char c){
+            this.c = c;
+            return this;
+        }
+        public builder setVal(int val){
+            this.val = val;
+            return this;
+        }
+        public builder setLeft(TreeNode left){
+            this.left = left;
+            return this;
+        }
+        public builder setRight(TreeNode right){
+            this.right = right;
+            return this;
+        }
+        public TreeNode build(){
+            return new TreeNode(this);
+        }
+    }
+    @Override
+    public int compareTo(TreeNode o) {
+        return this.val - o.val;
+    }
+}
